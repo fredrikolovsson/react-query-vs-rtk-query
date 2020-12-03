6e14983b39366f225a9edc8dd818df0ba690fa13
Fredrik Olovsson <fogithub@gmail.com>
2020-12-03 19:12:11 +0100

    implement advanced example from rtk-query
    with one query being used in two places, showing that it does not trigger duplicate requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './App.css'
+import { Pokemon } from './components/Pokemon'
+
+const pokemonNames = ['bulbasaur', 'pikachu', 'charmander']
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pokemon name="bulbasaur" />
+      {pokemonNames.map((name) => (
+        <Pokemon name={name} />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
